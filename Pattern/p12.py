585f5b02aa6b03167e30d95e3c597f9a1b8d179e
shivani-pansala <143687310+shivani-pansala@users.noreply.github.com>
2024-09-17 20:35:41 +0530

    Add files via upload

---
+++ Pattern/p12.py
@@ -0,0 +1,12 @@
+for i in range(1,6):
+     for s in range(5,i,-1):
+          print(end=" ")
+     for j in range(1,i+1):
+          print("*",end=" ")
+     print()
+for k in range(5,0,-1):
+     for l in range(5,k,-1):
+          print(end=" ")
+     for m in range(1,k+1):
+          print("*",end=" ")     
+     print()
